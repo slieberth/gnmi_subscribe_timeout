f8c5a1915bee339858a5a70899941e79d8076c8f
Stefan Lieberth <stefan@lieberth.net>
2021-01-20 22:26:07 +0100

    Update gnmi_subscribe_timeout.py

--- gnmi_subscribe_timeout.py
+++ gnmi_subscribe_timeout.py
@@ -6,22 +6,28 @@ import kthread
 
 from pygnmi.client import gNMIclient, telemetryParser
 
-class gnmi_timeout_iterator:
-    def __init__(self, gnmi_generator, timeout):
+class nonblocking_iterator:
+    def __init__(self, blocking_generator, timeout, stop_if_timeout = False):
         self.timeout = timeout
+        self.stop_if_timeout = stop_if_timeout
         self.msg_queue = queue.Queue()
-        self.gnmi_generator = gnmi_generator
+        self.blocking_generator = blocking_generator
         self._thread = kthread.KThread(
-            target=self.iter_gen)
+            target=self._put_messages_from_blocking_generator_to_queue)
         self._thread.start()
 
-    def iter_gen(self):
-        print("gnmi_gen started")
+    def _put_messages_from_blocking_generator_to_queue(self):
         while True:
-            self.msg_queue.put(next(self.gnmi_generator))
+            self.msg_queue.put(next(self.blocking_generator))
 
     def terminate(self):
         self._thread.terminate()
+        while self._thread.is_alive():
+            self._thread.join()
+        time.sleep(1)
+
+    def is_alive(self):
+        return self._thread.is_alive()
 
     def join(self):
         self._thread.join()
@@ -29,19 +35,25 @@ class gnmi_timeout_iterator:
     def __iter__(self):
         return self
 
-    def next(self):
+    def __next__(self):
         try:
             return self.msg_queue.get(timeout=self.timeout)
         except queue.Empty:
-            return None
+            self._thread.terminate()
+            while self._thread.is_alive():
+                self._thread.join()
+            time.sleep(1)
+            raise Exception("timeout error")
+
+    def next(self):
+        return self.__next__()
 
 if __name__ == '__main__':
 
-    # Aristas VM
     host = ('192.168.56.31',50051)
 
     sample_interval = 5
-    receiver_timeout = 7
+    recv_timeout = 3
 
     subscribe = {
                 'subscription': [
@@ -58,24 +70,96 @@ if __name__ == '__main__':
 
     gc = gNMIclient(
         target=host, username='admin', password='admin1', insecure=True)
+
     try:
         gc.__enter__()
         connect = True
     except:
         connect = False
-    if connect:
-        telemetry_subscribe_gen = gc.subscribe(subscribe=subscribe)
-        _iterator = gnmi_timeout_iterator(
-            telemetry_subscribe_gen, receiver_timeout)
 
-        while True:
-            _msg = _iterator.next()
-            if _msg == None:
-                _iterator.terminate()
-                gc.close()
-                raise Exception("gnmi receiver timeout")
-                break
-            else:
-                print(
-                    yaml.dump(telemetryParser(_msg),default_flow_style=False))
-        _iterator.join()
+    if connect:
+        print ("#1#"*30)
+        pygnmi_generator = gc.subscribe(subscribe=subscribe)
+        print ("#1#"*30)
+        _iter = nonblocking_iterator(pygnmi_generator, 3)
+        print (f"_iter : {_iter}")
+        try:
+            while True:
+                _msg = _iter.next()
+                if _msg:
+                    print(
+                        yaml.dump(telemetryParser(
+                            _msg),default_flow_style=False))
+        except Exception as exc:
+            print (f"got exception: {exc}")
+
+
+        print ("#2#"*30)
+        pygnmi_generator = gc.subscribe(subscribe=subscribe)
+        print ("#2#"*30)
+        _iter = nonblocking_iterator(pygnmi_generator, 7)
+        print (f"_iter : {_iter}")
+        try:
+            i = 0
+            while True:
+                i += 1
+                print (f"gnmi iteration {i}")
+                _msg = _iter.next()
+                if _msg == None:
+                    print ("gnmi receiver timeout")
+                    collecting = False
+                else:
+                    _parsed = telemetryParser(_msg)
+                    print (f"parsed: {_parsed}")
+                if i >= 10:
+                    #_iter.terminate()
+                    # _iter.join()
+                    break
+            _iter.terminate()
+        except Exception as exc:
+            print (f"!!! got exception: {exc}")
+
+        print ("#3#"*30)
+        pygnmi_generator = gc.subscribe(subscribe=subscribe)
+        print ("#3#"*30)
+        _iter = nonblocking_iterator(pygnmi_generator, 3)
+        print (f"_iter : {_iter}")
+        try:
+            i = 0
+            while True:
+                i += 1
+                print (f"gnmi iteration {i}")
+                _msg = _iter.next()
+                if _msg == None:
+                    print ("gnmi receiver timeout")
+                    collecting = False
+                else:
+                    _parsed = telemetryParser(_msg)
+                    print (f"parsed: {_parsed}")
+                    _synced = _parsed.get("sync_response",False)
+                    if _synced == True:
+                        print ("gnmi receiver synced")
+                        print ("gnmi iterator terminated")
+                        break
+                if i >= 10:
+                    break
+            _iter.terminate()
+            # _iter.join()
+        except Exception as exc:
+            print (f"got exception: {exc}")
+
+        print ("#4#"*30)
+        pygnmi_generator = gc.subscribe(subscribe=subscribe)
+        print ("#4#"*30)
+        try:
+            for i, _msg in enumerate(nonblocking_iterator(pygnmi_generator, 3)):
+                    if _msg == None:
+                        print ("gnmi receiver timeout")
+                        collecting = False
+                    else:
+                        _parsed = telemetryParser(_msg)
+                        print (f"parsed: {_parsed}")
+        except Exception as exc:
+            print (f"got exception: {exc}")
+        print ("#5#"*30)
+        # gc.close()
